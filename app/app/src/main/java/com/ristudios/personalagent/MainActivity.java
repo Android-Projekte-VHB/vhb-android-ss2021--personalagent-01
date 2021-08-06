64752564471f89858bac52e0df8846b3b3203ea7
RandomIntStudios <RandomIntStudios@gmail.com>
2021-08-06 22:23:41 +0200

    Added NotificationHelper.class and a testIcon
    NotificationHelper takes care of Notifications and provides methods to simply create a Notification, create a PendingIntent or to show a notification

--- app/app/src/main/java/com/ristudios/personalagent/MainActivity.java
+++ app/app/src/main/java/com/ristudios/personalagent/MainActivity.java
@@ -2,13 +2,18 @@ package com.ristudios.personalagent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.os.Bundle;
+
+import com.ristudios.personalagent.utils.NotificationHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        NotificationHelper notificationHelper = new NotificationHelper(this);
+        notificationHelper.createNotificationChannel(this, "Main Channel", "Handles all notifications", NotificationHelper.MAIN_NOTIFICATION_CHANNEL_ID);
         setContentView(R.layout.activity_main);
     }
 }
