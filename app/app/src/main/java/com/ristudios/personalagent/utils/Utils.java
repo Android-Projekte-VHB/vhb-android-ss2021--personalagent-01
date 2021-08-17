ecc6b1f0e33a276f9a3953c7b70994e9a4890dab
RandomIntStudios <RandomIntStudios@gmail.com>
2021-08-18 01:12:40 +0200

    Update Utils.java

--- app/app/src/main/java/com/ristudios/personalagent/utils/Utils.java
+++ app/app/src/main/java/com/ristudios/personalagent/utils/Utils.java
@@ -4,6 +4,7 @@ package com.ristudios.personalagent.utils;
 
 public final class Utils {
 
-
+    private static final String NOTIFICATION_MORNING_ENABLED = "notificationsMorningKey";
+    private static final String NOTIFICATION_EVENING_ENABLED = "notificationsEveningKey";
 
 }
